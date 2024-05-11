1fec4dde73856d4461b63d2273e93f78bc4acdd8
boreasu <acuareptor@gmail.com>
2024-05-10 21:23:58 -0500

    cambio

--- POO_GAME/src/Main.java
+++ POO_GAME/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-
+        //editador por Stephano
         Toyota toyo = new Toyota("PAS201"); //200
         toyo.encenderMotor();
         toyo.avanzar();
